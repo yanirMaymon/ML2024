9c741a064c7a2cee25861177cee17f8170a74881
yanirMaymon <yayamaymon4@gmail.com>
2024-01-31 22:54:54 +0200

    update submission

--- Ass1/ML_DL_Functions1.py
+++ Ass1/ML_DL_Functions1.py
@@ -38,20 +38,20 @@ def linear_regression_coeff_submission():
     seperated by commas
     :return: list of coefficiants for the linear regression problem.  
   '''
-  return [0, 0, 0, 0,
- 0, 0, 0, 0,
- 0, 0, 0, 0,
- 0, 0, 0, 0,
- 0, 0, 0, 0,
- 0, 0, 0, 0,
- 0, 0, 0, 0]
+  return [-5.60104810e-02, 3.73959200e-02, -3.33727853e-02,  3.95450109e-02,
+ -3.08215127e-02, -2.41596486e-03, 7.43219163e-02,  1.58064254e-02,
+  1.62477934e-02, -4.77338730e-02, 4.97174216e-02,  2.23789046e-02,
+  9.69579270e-02, 1.62581810e-01, 7.53441574e-01,  2.35805279e-02,
+  2.15328986e-02, 1.77235285e-02, 4.71508462e-03, -4.05292159e-04,
+  3.06129660e-02, 2.41223836e-02, 1.36845964e-02, -3.43531339e-02,
+ -3.03696739e-02, 4.01066341e-02, -2.82291201e-02, -3.73894721e-02]
 
 def linear_regression_intrcpt_submission():
   '''
     copy the intercept value from your notebook into here.
     :return: the intercept value.  
   '''
-  return 0
+  return -1.2759414611776407e-16
 
 def classification_coeff_submission():
   '''
