c5c758c5d90e2dc91501c1adf740d4452f3e1902
yanirMaymon <yayamaymon4@gmail.com>
2024-09-07 21:25:23 +0300

    Ass3 2.2

--- Ass3/ML_DL_Functions3.py
+++ Ass3/ML_DL_Functions3.py
@@ -72,6 +72,15 @@ class CNN(nn.Module):
 class CNNChannel(nn.Module):
     def __init__(self):# Do NOT change the signature of this function
         super(CNNChannel, self).__init__()
+        self.n = 32
+        kernel_size = 5
+        padding = (kernel_size - 1) / 2
+        self.conv1 = nn.Conv2d(in_channels=6,out_channels=self.n,kernel_size=kernel_size,padding=padding)
+        self.conv2 = nn.Conv2d(in_channels=self.n,out_channels=2*self.n,kernel_size=kernel_size,padding=padding)
+        self.conv4 = nn.Conv2d(in_channels=2*self.n,out_channels=4*self.n,kernel_size=kernel_size,padding=padding)
+        self.conv8 = nn.Conv2d(in_channels=4*self.n,out_channels=8*self.n,kernel_size=kernel_size,padding=padding)
+        self.fc1 = nn.Linear(14 * 14 * 8 * self.n,100)
+        self.fc2 = nn.Linear(100,2)
         # TODO: complete this method
 
     # TODO: complete this class
@@ -91,5 +100,29 @@ class CNNChannel(nn.Module):
             2 := same/different pair
         '''
         # TODO start by changing the shape of the input to (N,6,224,224)
+        inp = torch.cat([inp[:, :, :224, :], inp[:, :, 224:, :]], dim=1)
         # TODO: complete this function
-        return ...
+        out = self.conv1(inp)
+        out = F.relu(out)
+        out = F.max_pool2d(out, kernel_size=2)
+
+        out = self.conv2(out)
+        out = F.relu(out)
+        out = F.max_pool2d(out, kernel_size=2)
+
+        out = self.conv4(out)
+        out = F.relu(out)
+        out = F.max_pool2d(out, kernel_size=2)
+
+        out = self.conv8(out)
+        out = F.relu(out)
+        out = F.max_pool2d(out, kernel_size=2)
+
+        out = out.view(-1, 8 * self.n * 14 * 14)
+        out = self.fc1(out)
+        out = F.relu(out)
+
+        out = self.fc2(out)
+        out = F.softmax(out, dim=1)
+
+        return out
